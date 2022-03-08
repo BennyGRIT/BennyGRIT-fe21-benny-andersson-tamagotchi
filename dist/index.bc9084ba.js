7b7da0597fa1c8ec1b686b223a9862d7ae01da65
BennyGRIT <benny.andersson@gritacademy.se>
2022-03-08 19:31:09 +0100

    upload lots of parcels

--- dist/index.bc9084ba.js
+++ dist/index.bc9084ba.js
@@ -1,5 +1,11 @@
-let yourName = document.querySelector(".tamaName");
-console.log(yourName + "Benny");
+let yourName = document.getElementById("tamaName");
+console.log(yourName.value);
+let myBtn = document.getElementById("mySubmit");
+// let myBtn:any = (<HTMLInputElement>document.getElementById("mySubmit"))
+myBtn.addEventListener("click", function() {
+    console.log(yourName.value);
+    document.getElementById("hitNamn").innerHTML = yourName.value;
+});
 class Newtamagotchi {
     constructor(tamagotchiName, tamagotchiAnimal){
         this.tamagotchiName = tamagotchiName;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-let yourName = document.querySelector(".tamaName")
-console.log(yourName + "Benny")
+let yourName:any = (<HTMLInputElement>document.getElementById("tamaName"))
+console.log(yourName.value)
 
+let myBtn:any = (document.getElementById("mySubmit"))
+// let myBtn:any = (<HTMLInputElement>document.getElementById("mySubmit"))
 
-
+myBtn.addEventListener("click", function(){
+    console.log(yourName.value)
+    document.getElementById("hitNamn").innerHTML = yourName.value
+})
 
 class Newtamagotchi {
     constructor(

02defe3ed6d4c7c949a8dceed33dfdf4d7d14d86
BennyGRIT <benny.andersson@gritacademy.se>
2022-03-08 23:45:41 +0100

    Making constructor classes

--- dist/index.bc9084ba.js
+++ dist/index.bc9084ba.js
@@ -1,16 +1,23 @@
 let yourName = document.getElementById("tamaName");
-console.log(yourName.value);
+let yourAnimal = document.getElementById("tamaAnimal");
+console.log("Benny");
 let myBtn = document.getElementById("mySubmit");
 // let myBtn:any = (<HTMLInputElement>document.getElementById("mySubmit"))
-myBtn.addEventListener("click", function() {
-    console.log(yourName.value);
-    document.getElementById("hitNamn").innerHTML = yourName.value;
-});
-class Newtamagotchi {
-    constructor(tamagotchiName, tamagotchiAnimal){
-        this.tamagotchiName = tamagotchiName;
-        this.tamagotchiAnimal = tamagotchiAnimal;
+myBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    // let bennyPic = document.getElementById("hitPic") as HTMLImageElement;
+    // bennyPic.src = ".tamabenny.png"
+    class Newtamagotchi {
+        constructor(tamagotchiName, tamagotchiAnimal){
+            this.tamagotchiName = tamagotchiName;
+            this.tamagotchiAnimal = tamagotchiAnimal;
+        }
     }
-}
+    const myNewTamagotchi = new Newtamagotchi(yourName.value, yourAnimal.value);
+    myNewTamagotchi.tamagotchiName;
+    console.log(myNewTamagotchi);
+    document.getElementById("hitNamn").innerHTML = "Ditt namn: " + myNewTamagotchi.tamagotchiName;
+    document.getElementById("hitAnimal").innerHTML = "Ditt djur: " + myNewTamagotchi.tamagotchiAnimal;
+});
 
 //# sourceMappingURL=index.bc9084ba.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,33 @@
 let yourName:any = (<HTMLInputElement>document.getElementById("tamaName"))
-console.log(yourName.value)
+let yourAnimal:any = (<HTMLInputElement>document.getElementById("tamaAnimal"))
+
+console.log("Benny")
 
 let myBtn:any = (document.getElementById("mySubmit"))
 // let myBtn:any = (<HTMLInputElement>document.getElementById("mySubmit"))
 
-myBtn.addEventListener("click", function(){
-    console.log(yourName.value)
-    document.getElementById("hitNamn").innerHTML = yourName.value
-})
 
-class Newtamagotchi {
-    constructor(
-        public tamagotchiName: string,
-        public tamagotchiAnimal: string
-    ) {
+myBtn.addEventListener("click", function(e:any){
+    e.preventDefault();
+    // let bennyPic = document.getElementById("hitPic") as HTMLImageElement;
+    // bennyPic.src = ".tamabenny.png"
+
+    class Newtamagotchi {
+        constructor(
+            public tamagotchiName: any,
+            public tamagotchiAnimal: any
+        ) {
+        }
     }
-}
+    
+    const myNewTamagotchi = new Newtamagotchi (yourName.value, yourAnimal.value);
+    myNewTamagotchi.tamagotchiName
+    console.log(myNewTamagotchi)
+    
+    document.getElementById("hitNamn").innerHTML = "Ditt namn: "+ myNewTamagotchi.tamagotchiName
+    document.getElementById("hitAnimal").innerHTML = "Ditt djur: "+ myNewTamagotchi.tamagotchiAnimal
 
+
+    
+})
 
